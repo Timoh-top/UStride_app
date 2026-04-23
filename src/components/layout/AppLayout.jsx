cf32b5dec32dbfa6e61eae613bd676670f736a9a
Timoh-top <timoajewole@gmail.com>
2026-04-23 00:10:28 -0700

    fix navbar import casing

--- src/components/layout/AppLayout.jsx
+++ src/components/layout/AppLayout.jsx
@@ -1,12 +1,13 @@
 import React from "react";
-import Navbar from "./NavBar";
+import Navbar from "./Navbar";
 
 const AppLayout = ({ children }) => {
   return (
     <div className="min-h-screen bg-[#0b0f17] text-white">
       <Navbar />
-      
-      <main className="pt-20 px-4 md:px-10">
+
+      {/* IMPORTANT: prevents content hiding behind fixed navbar */}
+      <main className="pt-24 px-4 md:px-10">
         {children}
       </main>
     </div>
